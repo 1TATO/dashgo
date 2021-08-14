9f5a9973d4871cb2144a79d5e4f01a4bee94aca0
José Neto <josenetopr5@gmail.com>
2021-08-14 17:48:22 -0300

    Responsive Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,20 @@
 import { Flex, Box, Avatar, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box marginRight="4" textAlign="right">
-        <Text>Jose Neto</Text>
-        <Text
-          color="gray.300"
-          fontSize="small">josenetopr5@gmail.com</Text
-        >
-      </Box>
+      {showProfileData && (
+        <Box marginRight="4" textAlign="right">
+          <Text>Jose Neto</Text>
+          <Text color="gray.300" fontSize="small">
+            josenetopr5@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Jose Neto" src="https://github.com/1TATO.png" />
     </Flex>
